e619a597c00f50e82771446cf32544406eb5c4b5
Darshan Saini <50915527+darshansaini@users.noreply.github.com>
2023-01-21 17:44:48 +0530

    update

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,18 @@
 // declare class
 
 // export class using module.exports
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+
+    drawShape() {
+        console.log("Drawing a shape");
+    }
+
+    calculateArea() {
+        console.log("Calculating area of shape");
+    }
+}
+
+module.exports = Shape;
